743ced463d5cef59fdb8fc50ce3048c4e450455b
Jordan Caudill <jacaudill74@gmail.com>
2018-01-12 19:22:20 -0600

    chart

---
+++ public/chart.js
@@ -0,0 +1,166 @@
+var component = Vue.component('chart',
+	{
+		methods: {
+			getRandomColor: function () {
+				var colors = [
+					'rgb(252, 255, 7)',
+					'rgb(242, 186, 249)',
+					'rgb(78, 242, 93)',
+					'rgb(24, 153, 244)',
+					'rgb(255, 156, 0)',
+					'rgb(233, 58, 67)'
+				];
+				var color = colors[Math.floor(Math.random() * colors.length)];
+				while (color === $('.section.skills')[0].style['background-color']) {
+					color = colors[Math.floor(Math.random() * colors.length)];
+				}
+				return color;
+			}
+		},
+		mounted() {
+			var component = this;
+			let data = [
+				{ name: 'Angular', value: 5, icon: 'Angular' },
+				{ name: 'NodeJS', value: 5, icon: 'NodeJS' },
+				{ name: 'MongoDB', value: 3, icon: 'MongoDB' },
+				{ name: 'Javascript', value: 4, icon: 'Javascript' },
+				{ name: 'SASS', value: 1, icon: 'SASS' },
+				{ name: 'LESS', value: 1, icon: 'LESS' },
+				{ name: 'Express', value: 4, icon: 'Express' },
+				{ name: 'AJAX', value: 1, icon: 'AJAX' },
+				{ name: 'HTML5', value: 3, icon: 'HTML5' },
+				{ name: 'Vue.js', value: 3, icon: 'Vue.js' },
+				{ name: 'MySQL', value: 2, icon: 'MySQL' },
+				{ name: 'CSS', value: 4, icon: 'CSS' },				
+				{ name: 'Linux', value: 1, icon: 'Linux' },				
+				{ name: 'Webpack', value: 3, icon: 'Webpack' },				
+				{ name: 'Jasmine', value: 3, icon: 'Jasmine' },				
+				{ name: 'Mocha', value: 3, icon: 'Mocha' },				
+				{ name: 'Karma', value: 3, icon: 'Karma' },				
+				{ name: 'jQuery', value: 2, icon: 'jQuery' },				
+
+			];
+			let svg = d3.select('#chart');
+			let width = svg.property('clientWidth'); // get width in pixels
+			let height = +svg.attr('height');
+			let centerX = width * 0.5;
+			let centerY = height * 0.5;
+			let strength = 0.05;
+			let focusedNode;
+
+			let format = d3.format(',d');
+
+
+			// use pack to calculate radius of the circle
+			let pack = d3.pack()
+				.size([width, height])
+				.padding(1.5);
+
+			let forceCollide = d3.forceCollide(d => d.r + 1);
+
+			// use the force
+			let simulation = d3.forceSimulation()
+				// .force('link', d3.forceLink().id(d => d.id))
+				.force('charge', d3.forceManyBody())
+				.force('collide', forceCollide)
+				// .force('center', d3.forceCenter(centerX, centerY))
+				.force('x', d3.forceX(centerX).strength(strength))
+				.force('y', d3.forceY(centerY).strength(strength));
+
+			// reduce number of circles on mobile screen due to slow computation
+			if ('matchMedia' in window && window.matchMedia('(max-device-width: 767px)').matches) {
+				data = data.filter(el => {
+					return el.value >= 50;
+				});
+			}
+
+			let root = d3.hierarchy({ children: data })
+				.sum(d => d.value);
+
+
+			// we use pack() to automatically calculate radius conveniently only
+			// and get only the leaves
+			let nodes = pack(root).leaves().map(node => {
+				const data = node.data;
+				return {
+					x: centerX + (node.x - centerX) * 3, // magnify start position to have transition to center movement
+					y: centerY + (node.y - centerY) * 3,
+					r: 0, // for tweening
+					radius: node.r, //original radius
+					name: data.name,
+					value: data.value,
+					icon: data.icon
+				};
+			});
+			simulation.nodes(nodes).on('tick', ticked);
+
+			let node = svg.selectAll('.node')
+				.data(nodes)
+				.enter().append('g')
+				.attr('class', 'node')
+				.call(d3.drag()
+					.on('start', (d) => {
+						if (!d3.event.active) { simulation.alphaTarget(0.2).restart(); }
+						d.fx = d.x;
+						d.fy = d.y;
+					})
+					.on('drag', (d) => {
+						d.fx = d3.event.x;
+						d.fy = d3.event.y;
+					})
+					.on('end', (d) => {
+						if (!d3.event.active) { simulation.alphaTarget(0); }
+						d.fx = null;
+						d.fy = null;
+					}));
+			node.append('circle')
+				.attr('id', d => d.id)
+				.attr('r', 0)
+				.style('fill', d => component.getRandomColor())
+				.transition().duration(2000).ease(d3.easeElasticOut)
+				.tween('circleIn', (d) => {
+					let i = d3.interpolateNumber(0, d.radius);
+					return (t) => {
+						d.r = i(t);
+						simulation.force('collide', forceCollide);
+					};
+				});
+
+			node.append('clipPath')
+				.attr('id', d => `clip-${d.id}`)
+				.append('use')
+				.attr('xlink:href', d => `#${d.id}`);
+
+			// display text as circle icon
+			node.append('text')
+				.selectAll('tspan')
+				.data((d, i) => {
+					return d.icon.split(';')
+				})
+				.enter()
+				.append('tspan')
+				.text(data => {
+					return data;
+				});
+			node.select('tspan').attr('x', (d, i, p) => {
+				return nodes[i].name.length * (nodes[i].radius / 3) * -1 / 4;
+			});
+			node.style('font-size', (d, i) => {
+				return nodes[i].radius / 3;
+			});
+			node.style('text-align', 'center');
+			node.style('cursor', 'pointer');
+
+			function ticked() {
+				node
+					.attr('transform', d => `translate(${d.x},${d.y})`)
+					.select('circle')
+					.attr('r', d => d.r);
+			}
+
+		},
+		template: '<svg id="chart" width="100%" height="' + window.innerHeight + '"></svg>'
+	}
+);
+
+
--- public/script.js
+++ public/script.js
@@ -2,21 +2,45 @@ var app = new Vue({
     el: '#app',
     data: {
         colors: [
-            '#fcff07',
-            '#f2baf9',
-            '#4ef25d',
-            '#1899f4',
-            '#ff9c00',
-            '#e93a43'
-        ]
+            'rgb(252, 255, 7)',
+            'rgb(242, 186, 249)',
+            'rgb(78, 242, 93)',
+            'rgb(24, 153, 244)',
+            'rgb(255, 156, 0)',
+            'rgb(233, 58, 67)'
+        ],
+        age: moment('03/13/1995').diff(moment(), 'years') * -1,
+        fitbitUserID: '5MMGDK',
+        // get token (1 year) here: https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL68&scope=activity%20heartrate%20sleep&expires_in=31536000        
+        fitbitRequestOptions: {
+            headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1TU1HREsiLCJhdWQiOiIyMkNMNjgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJociByc2xlIiwiZXhwIjoxNTQ3MzE1ODM5LCJpYXQiOjE1MTU3Nzk4Mzl9.iLhOl3s2-qZGRHPsKUlSudh5Ks-4nnCP61Zt3AanzZY' }
+        },
+        steps: '4,023',
+        heartrate: 68,
+        sleep: 8.2
     },
     methods: {
         getRandomColor: function () {
             return this.colors[Math.floor(Math.random() * this.colors.length)];
-        }
+        },
+        getActivity: function () {
+            fetch('https://api.fitbit.com/1/user/' + this.fitbitUserID + '/activities/date/' + moment().format('YYYY-MM-DD') + '.json', this.fitbitRequestOptions)
+                .then(response => response.json())
+                .then(json => {
+                    this.steps = json.summary.steps.toLocaleString();
+                    this.heartrate = json.summary.restingHeartRate
+                });
+            fetch('https://api.fitbit.com/1/user/' + this.fitbitUserID + '/sleep/date/' + moment().subtract(4, 'days').format('YYYY-MM-DD') + '.json', this.fitbitRequestOptions)
+                .then(response => response.json())
+                .then(json => {
+                    this.sleep = (json.summary.totalMinutesAsleep / 60).toFixed(1);
+                });
+
+        },
     },
     created() {
         var sections = $('.section');
+        // this.getActivity();
         for (var i = 0; i < sections.length; i++) {
             var color = this.getRandomColor();
             sections[i].style.backgroundColor = color;
