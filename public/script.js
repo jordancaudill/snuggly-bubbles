742fc3926d1ccf591b3936642bda6cb6cf7b3fd0
Jordan Caudill <jacaudill74@gmail.com>
2018-07-23 08:40:16 -0500

    fx

--- public/script.js
+++ public/script.js
@@ -1,23 +1,25 @@
 var app = new Vue({
 	el: '#app',
-	data: {
-		colors: [
-			'rgb(252, 255, 7)',
-			'rgb(242, 186, 249)',
-			'rgb(78, 242, 93)',
-			'rgb(24, 153, 244)',
-			'rgb(255, 156, 0)',
-			'rgb(233, 58, 67)'
-		],
-		age: moment('03/13/1995').diff(moment(), 'years') * -1,
-		fitbitUserID: '5MMGDK',
-		// get token (1 year) here: https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL68&scope=activity%20heartrate%20sleep&expires_in=31536000        
-		fitbitRequestOptions: {
-			headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1TU1HREsiLCJhdWQiOiIyMkNMNjgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJociByc2xlIiwiZXhwIjoxNTQ3MzE1ODM5LCJpYXQiOjE1MTU3Nzk4Mzl9.iLhOl3s2-qZGRHPsKUlSudh5Ks-4nnCP61Zt3AanzZY' }
-		},
-		steps: '4,023',
-		heartrate: 68,
-		sleep: 8.2
+	data: function () {
+		return {
+			colors: [
+				'rgb(252, 255, 7)',
+				'rgb(242, 186, 249)',
+				'rgb(78, 242, 93)',
+				'rgb(24, 153, 244)',
+				'rgb(255, 156, 0)',
+				'rgb(233, 58, 67)'
+			],
+			age: moment('03/13/1995').diff(moment(), 'years') * -1,
+			fitbitUserID: '5MMGDK',
+			// get token (1 year) here: https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL68&scope=activity%20heartrate%20sleep&expires_in=31536000        
+			fitbitRequestOptions: {
+				headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1TU1HREsiLCJhdWQiOiIyMkNMNjgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJociByc2xlIiwiZXhwIjoxNTQ3MzE1ODM5LCJpYXQiOjE1MTU3Nzk4Mzl9.iLhOl3s2-qZGRHPsKUlSudh5Ks-4nnCP61Zt3AanzZY' }
+			},
+			steps: '4,023',
+			heartrate: 68,
+			sleep: 8.2
+		}
 	},
 	methods: {
 		getRandomColor: function () {
@@ -74,8 +76,8 @@ var app = new Vue({
 				selector: '.particle-bg',
 				color: 'white',
 				sizeVariations: 10
-			  });
-			
+			});
+
 			$('#fullpage').fullpage({
 				afterLoad: function (anchorLink, index) {
 					$('#fullpage')[0].style.opacity = 1;
