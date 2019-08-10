7af17ba6a85dd3707868615fae71981169db91f0
Jordan Caudill <jcaudill@disro.com>
2019-08-09 22:18:42 -0500

    for github hosting

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// console.log(window)
 var app = new Vue({
 	el: '#app',
 	data: function () {
@@ -10,51 +11,15 @@ var app = new Vue({
 				'rgb(255, 156, 0)',
 				'rgb(233, 58, 67)'
 			],
-			age: moment('03/13/1995').diff(moment(), 'years') * -1,
-			fitbitUserID: '5MMGDK',
-			// get token (1 year) here: https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22CL68&scope=activity%20heartrate%20sleep&expires_in=31536000        
-			fitbitRequestOptions: {
-				headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1TU1HREsiLCJhdWQiOiIyMkNMNjgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJociByc2xlIiwiZXhwIjoxNTQ3MzE1ODM5LCJpYXQiOjE1MTU3Nzk4Mzl9.iLhOl3s2-qZGRHPsKUlSudh5Ks-4nnCP61Zt3AanzZY' }
-			},
-			steps: '4,023',
-			heartrate: 68,
-			sleep: 8.2
+			age: moment('03/13/1995').diff(moment(), 'years') * -1
 		}
 	},
 	methods: {
 		getRandomColor: function () {
 			return this.colors[Math.floor(Math.random() * this.colors.length)];
-		},
-		getActivity: function () {
-			fetch('https://api.fitbit.com/1/user/' + this.fitbitUserID + '/activities/date/' + moment().format('YYYY-MM-DD') + '.json', this.fitbitRequestOptions)
-				.then(response => response.json())
-				.then(json => {
-					if (json.summary.steps == 0) {
-						this.steps = '1,502';
-					} else {
-						this.steps = json.summary.steps.toLocaleString();
-					}
-					if (json.summary.restingHeartRate == 0) {
-						this.heartrate = 68;
-					} else {
-						this.heartrate = json.summary.restingHeartRate;
-					}
-				});
-			fetch('https://api.fitbit.com/1/user/' + this.fitbitUserID + '/sleep/date/' + moment().subtract(4, 'days').format('YYYY-MM-DD') + '.json', this.fitbitRequestOptions)
-				.then(response => response.json())
-				.then(json => {
-					if (json.summary.totalMinutesAsleep == 0) {
-						this.sleep = 7.8;
-					} else {
-						this.sleep = (json.summary.totalMinutesAsleep / 60).toFixed(1);
-					}
-				});
-
-		},
-	},
+		}	},
 	created() {
 		let vm = this;
-		this.getActivity();
 		var sections = $('.section');
 		var pattern = Trianglify({
 			width: window.innerWidth,
@@ -69,14 +34,14 @@ var app = new Vue({
 			height: window.innerHeight
 		});
 		pnguri = pattern.png();
-		sections[4].style['background'] = "url('" + pnguri + "') no-repeat center center";
-		sections[4].style['background-size'] = "cover";
+		// sections[4].style['background'] = "url('" + pnguri + "') no-repeat center center";
+		// sections[4].style['background-size'] = "cover";
 		$(document).ready(function () {
-			Particles.init({
-				selector: '.particle-bg',
-				color: 'white',
-				sizeVariations: 10
-			});
+			// pJS.init({
+			// 	selector: '.particle-bg',
+			// 	color: 'white',
+			// 	sizeVariations: 10
+			// });
 
 			$('#fullpage').fullpage({
 				afterLoad: function (anchorLink, index) {
